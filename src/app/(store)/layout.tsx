73a29e8a6c8aa493409e726cf5ac874940309087
Johnfavour-maidu <maidujohn1@gmail.com>
2026-06-08 09:23:07 +0100

    revert: header/footer redesign, checkout, wishlist, support pages

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,5 +3,11 @@ export default function StoreLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen pt-20 pb-12">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    </div>
+  );
 }
